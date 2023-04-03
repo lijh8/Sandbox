08949b80bab113601a4271368d2cea5e1779c31e
ljhm <9011764+ljhm@users.noreply.github.com>
2023-04-04 00:35:49 +0800

    Add files via upload

---
+++ Sandbox/hello_swift/hello_swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello_swift
+//
+//  Created by ljh on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
